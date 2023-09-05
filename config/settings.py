62b6941f30b16e53f5a963a2baef1b88d39c593c
eugreg <daiaegregory@gmail.com>
2023-09-05 09:07:40 -0300

    apagando banco

--- config/settings.py
+++ config/settings.py
@@ -12,18 +12,23 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+load_dotenv()
 
+MODE = os.getenv("MODE")
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
+SECRET_KEY = os.getenv("SECRET_KEY")
+DEBUG = os.getenv("DEBUG", "False")
+ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ["http://localhost:3000", "http://localhost:8000", "https://*.fl0.io/"]
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = (
     "django-insecure-o$yihd5u#3b7@i7enym#u5kqwd+t++j4j548f*&oqstr#bs^$#"
 )
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
