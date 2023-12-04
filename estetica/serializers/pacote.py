6706d009665330c453b8768a395d6deea5e58308
Mylene Tomasi <99373744+mylenetomasi@users.noreply.github.com>
2023-12-04 18:55:27 -0300

    Update pacote.py (teste)

--- estetica/serializers/pacote.py
+++ estetica/serializers/pacote.py
@@ -4,15 +4,16 @@ from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 class PacoteSerializer(ModelSerializer):
+    imagem_attachment_key = SlugRelatedField(
+        source="imagem",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
+        
     class Meta:
-        imagem_attachment_key = SlugRelatedField(
-            source="imagem",
-            queryset=Image.objects.all(),
-            slug_field="attachment_key",
-            required=False,
-            write_only=True,
-        )
-        imagem = ImageSerializer(required=False, read_only=True)
         model = Pacote
         fields= "__all__"
 
