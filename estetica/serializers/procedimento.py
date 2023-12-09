64fc80e2c3b0073c07c953779d3458dc82928954
Mylene Tomasi <99373744+mylenetomasi@users.noreply.github.com>
2023-12-09 18:46:00 -0300

    Update procedimento.py

--- estetica/serializers/procedimento.py
+++ estetica/serializers/procedimento.py
@@ -4,15 +4,15 @@ from uploader.models import Image
 from uploader.serializers import ImageSerializer
 
 class ProcedimentoSerializer(ModelSerializer):
+    imagem_attachment_key = SlugRelatedField(
+        source="imagem",
+        queryset=Image.objects.all(),
+        slug_field="attachment_key",
+        required=False,
+        write_only=True,
+    )
+    imagem = ImageSerializer(required=False, read_only=True)
     class Meta:
-        imagem_attachment_key = SlugRelatedField(
-            source="imagem",
-            queryset=Image.objects.all(),
-            slug_field="attachment_key",
-            required=False,
-            write_only=True,
-        )
-        imagem = ImageSerializer(required=False, read_only=True)
         model = Procedimento
         fields= "__all__"
 
