700de40aab87f5a5bc3c4dca941636c9ba5fff17
Mylene Tomasi <99373744+mylenetomasi@users.noreply.github.com>
2023-12-09 21:04:36 -0300

    Update urls.py

--- config/urls.py
+++ config/urls.py
@@ -6,7 +6,10 @@ from django.conf.urls.static import static
 
 from rest_framework.routers import DefaultRouter
 from uploader.router import router as uploader_router
-
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -33,6 +36,8 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(router.urls)),
     path("api/usuario", include(usuario_router.urls)),
+    path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
         "api/swagger/",
